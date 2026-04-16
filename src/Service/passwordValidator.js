d1491f19fcad4279b8abac6fb5ace30d03bd1263
Claudir Fantuci <claudirfantucijunior@gmail.com>
2026-04-16 17:35:10 -0300

    feat: adicionando validação de maiuscula

--- src/Service/passwordValidator.js
+++ src/Service/passwordValidator.js
@@ -2,6 +2,9 @@ function validatePassword(password) {
     if (password.length < 8) {
         return false;
     }
+    if (!/[A-Z]/.test(password)) {
+        return false;
+    }
     return true;
 }
 
